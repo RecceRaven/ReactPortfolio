5d517718def1042ea35148401197e888c604ecc0
RecceRaven <anthonyramdeen782@gmail.com>
2024-02-06 00:49:07 -0500

    adjusted navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import "../Header/header.css";
 
 const Header = () => {
+    const [isActive, setIsActive] = useState(false);
+    const getNavLinkClass = (isActive) => {
+        return isActive ? "navbar-item navbar-item-active" : "navbar-item";
+    };
+
     return (
         <header>
             <nav className="navbar is-fullwidth" role="navigation" aria-label="main navigation">
@@ -10,23 +15,29 @@ const Header = () => {
                     <a className="navbar-item" href="/">
                         Anthony Ramdeen
                     </a>
+                    {/* Hamburger menu button, shown only on mobile */}
+                    <a onClick={() => setIsActive(!isActive)} role="button" className={`navbar-burger ${isActive ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
                 </div>
 
-                <div id="navbar" className="navbar-menu">
+                <div id="navbarBasicExample" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
                     <div className="navbar-end">
-                        <NavLink
-                            to="/aboutme"
-                            className={({ isActive }) => isActive ? "navbar-item is-active" : "navbar-item"}>
+                        <NavLink to="/aboutme" className={({ isActive }) => getNavLinkClass(isActive)}>
                             About Me
                         </NavLink>
 
-                        <NavLink to="/portfolio" className={({ isActive }) => isActive ? "navbar-item is-active" : "navbar-item"}>
+                        <NavLink to="/portfolio" className={({ isActive }) => getNavLinkClass(isActive)}>
                             Portfolio
                         </NavLink>
-                        <NavLink to="/contact" className={({ isActive }) => isActive ? "navbar-item is-active" : "navbar-item"}>
+
+                        <NavLink to="/contact" className={({ isActive }) => getNavLinkClass(isActive)}>
                             Contact
                         </NavLink>
-                        <NavLink to="/resume" className={({ isActive }) => isActive ? "navbar-item is-active" : "navbar-item"}>
+
+                        <NavLink to="/resume" className={({ isActive }) => getNavLinkClass(isActive)}>
                             Resume
                         </NavLink>
                     </div>
